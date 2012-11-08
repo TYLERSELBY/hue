b6a8829642a3711faf5cb1ed8fc82b8ecf090c1b
Eric Betts <bettse@gmail.com>
2012-11-07 21:24:52 -0800

    Add the default hue profiles to light class

--- light.py
+++ light.py
@@ -36,3 +36,24 @@ class Light:
         r = requests.get(url)
         return json.loads(r.content)['state']
 
+    def concentrate(self):
+        body = json.dumps({u'on': True, u'hue': 13122, u'colormode': u'ct', u'effect': u'none', u'alert': u'none', u'xy': [0.5119, 0.4147], u'reachable': True, u'bri': 219, u'sat': 211, u'ct': 233})
+        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
+        r = requests.put(url, data=body)
+
+    def energize(self):
+        body = json.dumps({u'on': True, u'hue': 13122, u'colormode': u'ct', u'effect': u'none', u'alert': u'none', u'xy': [0.5119, 0.4147], u'reachable': True, u'bri': 203, u'sat': 211, u'ct': 156})
+        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
+        r = requests.put(url, data=body)
+
+    def reading(self):
+        body = json.dumps({u'on': True, u'hue': 13122, u'colormode': u'ct', u'effect': u'none', u'alert': u'none', u'xy': [0.5119, 0.4147], u'reachable': True, u'bri': 240, u'sat': 211, u'ct': 346})
+        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
+        r = requests.put(url, data=body)
+
+    def relax(self):
+        body = json.dumps({u'on': True, u'hue': 13122, u'colormode': u'ct', u'effect': u'none', u'alert': u'none', u'xy': [0.5119, 0.4147], u'reachable': True, u'bri': 144, u'sat': 211, u'ct': 467})
+        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
+        r = requests.put(url, data=body)
+
+
