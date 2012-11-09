dd0c104873a3e7f15cd3eaac0cab5ba66ba7f84c
Eric Betts <bettse@gmail.com>
2012-11-08 20:59:33 -0800

    create method for common code

--- light.py
+++ light.py
@@ -9,24 +9,26 @@ class Light:
         self.secret = secret
         self.lightnum = lightnum
 
+    def setstate(self, body):
+        print body
+        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
+        r = requests.put(url, data=body)
+
     def brightness(self, i):
         if(i == 'full'):
-            i = 255
-        if(i > 255):
-            i = 255
+            i = 254
+        if(int(i) > 254):
+            i = 254
         bri = json.dumps({'bri': i, 'on': True})
-        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
-        r = requests.put(url, data=bri)
+        self.setstate(bri)
 
     def on(self):
         body = json.dumps({'on': True})
-        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
-        r = requests.put(url, data=body)
+        self.setstate(body)
 
     def off(self):
         body = json.dumps({'on': False})
-        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
-        r = requests.put(url, data=body)
+        self.setstate(body)
 
     def number(self):
         return self.lightnum
@@ -38,22 +40,17 @@ class Light:
 
     def concentrate(self):
         body = json.dumps({u'on': True, u'hue': 13122, u'colormode': u'ct', u'effect': u'none', u'alert': u'none', u'xy': [0.5119, 0.4147], u'reachable': True, u'bri': 219, u'sat': 211, u'ct': 233})
-        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
-        r = requests.put(url, data=body)
+        self.setstate(body)
 
     def energize(self):
         body = json.dumps({u'on': True, u'hue': 13122, u'colormode': u'ct', u'effect': u'none', u'alert': u'none', u'xy': [0.5119, 0.4147], u'reachable': True, u'bri': 203, u'sat': 211, u'ct': 156})
-        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
-        r = requests.put(url, data=body)
+        self.setstate(body)
 
     def reading(self):
         body = json.dumps({u'on': True, u'hue': 13122, u'colormode': u'ct', u'effect': u'none', u'alert': u'none', u'xy': [0.5119, 0.4147], u'reachable': True, u'bri': 240, u'sat': 211, u'ct': 346})
-        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
-        r = requests.put(url, data=body)
+        self.setstate(body)
 
     def relax(self):
         body = json.dumps({u'on': True, u'hue': 13122, u'colormode': u'ct', u'effect': u'none', u'alert': u'none', u'xy': [0.5119, 0.4147], u'reachable': True, u'bri': 144, u'sat': 211, u'ct': 467})
-        url = 'http://%s/api/%s/lights/%s/state' % (self.ip, self.secret, self.lightnum)
-        r = requests.put(url, data=body)
-
+        self.setstate(body)
 
