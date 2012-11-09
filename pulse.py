e9b476617d5477c5d92ebd0e565214c3e7d2cfe4
Eric Betts <bettse@gmail.com>
2012-11-08 20:43:59 -0800

    pulse: add light parameter and number of pulses limit

--- pulse.py
+++ pulse.py
@@ -2,6 +2,7 @@
 from time import sleep
 import ConfigParser
 from light import Light
+import sys
 
 config = ConfigParser.RawConfigParser()
 config.read('hue.cfg')
@@ -11,12 +12,21 @@ secret = config.get('hue', 'secret')
 light = config.get('hue', 'light')
 numlights = int(config.get('hue', 'numlights'))
 
+if(len(sys.argv) > 1):
+    light = sys.argv[1]
+
+if(len(sys.argv) > 2):
+    x = int(sys.argv[2])
+else:
+    x = 100
+
 if light.strip() == 'all':
     lights = [Light(ip, secret, x) for x in range(1, numlights+1)]
 else:
     lights = [Light(ip, secret, light)]
 
-while 1:
+while x > 0:
+    x = x - 1
     for light in lights:
         #print "on", light.number()
         light.on()
