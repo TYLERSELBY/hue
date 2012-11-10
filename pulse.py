8e3e411dc3525cab8adb68807b9633523ccdc917
Eric Betts <bettse@gmail.com>
2012-11-09 21:09:33 -0800

    reflow defaults setting, only capture state once

--- pulse.py
+++ pulse.py
@@ -11,24 +11,23 @@ ip = config.get('hue', 'ip')
 secret = config.get('hue', 'secret')
 light = config.get('hue', 'light')
 numlights = int(config.get('hue', 'numlights'))
+x = 100
+color = None
 
 print "%s <lightnum> <pulse count> <color>" % sys.argv[0]
 
+print sys.argv
 if(len(sys.argv) > 1):
     light = sys.argv[1]
-
 if(len(sys.argv) > 2):
     x = int(sys.argv[2])
-else:
-    x = 100
-
 if(len(sys.argv) > 3):
     color = sys.argv[3]
-else:
-    color = None
+
+print light, x, color
 
 if light.strip() == 'all':
-    lights = [Light(ip, secret, x, True) for x in range(1, numlights+1)]
+    lights = [Light(ip, secret, i, True) for i in range(1, numlights+1)]
 else:
     lights = [Light(ip, secret, light, True)]
 
@@ -38,7 +37,8 @@ while x > 0:
     x = x - 1
     #Set to alternate state
     for light in lights:
-        state[light.number()] = light.getstate()
+        if(not state.has_key(light.number())):
+            state[light.number()] = light.getstate()
         #flash color
         if(color):
             getattr(light, color)()
