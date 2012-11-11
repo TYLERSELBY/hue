3c9eab7b0cbb48ea98fc71864bd8ab9248acae05
Eric Betts <bettse@gmail.com>
2012-11-10 21:08:25 -0800

    add command line options for fadeon

--- fadeon.py
+++ fadeon.py
@@ -4,6 +4,7 @@ from time import sleep
 import json
 import ConfigParser
 from light import Light
+import sys
 
 
 config = ConfigParser.RawConfigParser()
@@ -15,18 +16,33 @@ light = config.get('hue', 'light')
 numlights = int(config.get('hue', 'numlights'))
 
 duration=600
-max_bri=255
+max_bri=254
+
+
+print "%s <lightnum> <duration> <max_bri>" % sys.argv[0]
+
+if(len(sys.argv) > 1):
+    light = sys.argv[1]
+if(len(sys.argv) > 2):
+    duration = int(sys.argv[2])
+if(len(sys.argv) > 3):
+    max_bri = sys.argv[3]
+
+if(max_bri == 'full' or int(max_bri) > 254):
+    max_bri = 254
+else:
+    max_bri = int(max_bri)
 
 if light.strip() == 'all':
-    lights = [Light(ip, secret, x) for x in range(1, numlights+1)]
+    lights = [Light(ip, secret, i) for i in range(1, numlights+1)]
 else:
     lights = [Light(ip, secret, light)]
 
 interval = float(duration)/max_bri
 
-#Don't allow internal faster than 2/second
-if ( (interval / len(lights)) < .5):
-    interval = 0.5
+#Don't allow internal faster than 3/second
+if ( (interval / len(lights)) < .3):
+    interval = 0.3
 
 #turn defined lights on but dark
 for light in lights:
@@ -37,4 +53,3 @@ for i in range(1, max_bri):
     for light in lights:
         light.brightness(i)
         sleep(interval)
-        print "Light %s to level %s" % (light.number(), i)
